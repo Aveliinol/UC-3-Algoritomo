cbb3657b3fefbd943c73aa5ae7f6591d25315bbf
xexeu.l <lugabriell084@outlook.com.br>
2024-09-26 15:24:57 -0300

    Docs: Alterei o arquivo index.html e fiz até a questão 6 no arquivo script.js

---
+++ Atividade_8/script.js
@@ -0,0 +1,63 @@
+// Questão 1 
+
+// let numeros = [34, 7, 23, 32, 5, 62];
+// numeros.push(9, 12);
+// numeros.unshift(1);
+// numeros.sort((a, b) => a - b);
+// numeros.reverse();
+
+// console.log(numeros);
+
+// Questão 2
+ 
+// let saudacao = "Olá ";
+// let nome = "Mundo"; 
+// let msg = saudacao.concat(nome); 
+// let msgM = msg.toUpperCase();
+// let parte = msgM.slice(4);
+
+// console.log(parte);
+
+// Questão 3 
+
+// let valores = [10, 25, 30, 45, 50, 60];
+// let valor = valores.filter(function(numero){
+//     return numero > 30; 
+// })
+// let soma = valor.reduce(function(acumulador, numeros){
+//     return acumulador + numeros;
+// }); 
+
+// console.log(isNaN(soma));
+
+// Questão 4 
+
+// let numeros = [1, 2, 3, 4, 5];
+// let multiplicacao = numeros.map(
+// function(numero){
+//     return numero * 2; 
+// });
+// let soma = multiplicacao.reduce(function(acumulador, numeros){
+//         return acumulador + numeros;
+// });
+
+// console.log(soma);
+
+// Questão 5 
+
+// let frase = "A programação é divertida e desafiadora.";
+// // console.log(frase.length);
+// let maiusculo = frase.toUpperCase();
+// let frases = maiusculo.split(" ");
+// let frasesM = frases.join("-");
+
+// console.log(frasesM);
+
+// Questão 6 
+
+// let frutas = ["Maçã", "Banana", "Laranja", "Manga", "Uva"];
+// frutas.splice(2, 1, "Abacaxi");
+// frutas.splice(0, 3);
+// frutas.push("Melancia");
+
+// console.log(frutas);
